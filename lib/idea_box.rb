c89dddb80e5146b3b6cb97f82ee39c342cd12f29
Luke Martinez <lukemartinez@gmail.com>
2013-10-16 16:43:07 -0600

    I3: Everything done up to extensions

---
+++ lib/idea_box.rb
@@ -0,0 +1,6 @@
+require 'idea_box/idea'
+require 'idea_box/idea_store'
+
+class IdeaBoxApp < Sinatra::Base
+
+end
---
+++ lib/idea_box/idea.rb
@@ -0,0 +1,36 @@
+class Idea
+  include Comparable
+
+  attr_reader :title, :description, :rank, :id
+
+  def initialize(attributes = {})
+    @title = attributes["title"]
+    @description = attributes["description"]
+    @rank = attributes["rank"] || 0
+    @id = attributes["id"]
+  end
+
+  def <=>(other)
+    other.rank <=> rank
+  end
+
+  def save
+      IdeaStore.create(to_h)
+  end
+
+  def to_h
+    {
+      "title" => title,
+      "description" => description, 
+      "rank" => rank
+    }
+  end
+
+  def database
+    Idea.database   #<---- what is this doing?
+  end
+
+  def like!
+    @rank += 1
+  end
+end
